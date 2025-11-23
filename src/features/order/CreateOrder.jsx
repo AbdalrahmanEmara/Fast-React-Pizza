3e51ed09a01546600f1d34a3ccf423ba9dbe1320
Abdelrahman Emara <abdelrahmanemara155@gmail.com>
2025-11-23 22:57:29 +0200

    Styling form elements

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -6,7 +6,7 @@ import { createOrder } from "../../services/apiRestaurant";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
@@ -60,10 +60,16 @@ function CreateOrder() {
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
-        <div>new
+        <div>
+          new
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input
+              type="text"
+              name="address"
+              required
+              className="w-full rounded-full px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
+            />
           </div>
         </div>
 
@@ -72,23 +78,33 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="  // console.log(order);
-priority">Want to yo give your order priority?</label>
+          <label
+            htmlFor="  // console.log(order);
+priority"
+          >
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting} className="bg-yellow-400 uppercase font-semibold px-4 py-3 rounded-full hover:bg-yellow-300 text-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed">{isSubmitting ? "Placing order..." : "Order now"}</button>
+          <button
+            disabled={isSubmitting}
+            className="rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+          >
+            {isSubmitting ? "Placing order..." : "Order now"}
+          </button>
         </div>
       </Form>
     </div>
   );
 }
 
-export async function action({request}) {
+export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
 
@@ -98,11 +114,13 @@ export async function action({request}) {
     ...data,
     cart: JSON.parse(data.cart),
     priority: data.priority === "on",
-  }
+  };
 
   const errors = {};
 
-  if (!isValidPhone(order.phone)) errors.phone = "Please give us your correct phone number. We might need it to contact you.";
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      "Please give us your correct phone number. We might need it to contact you.";
 
   if (Object.keys(errors).length > 0) return errors;
 
--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchOrder() {
   const [query, setQuery] = useState("");
@@ -14,7 +14,12 @@ export default function SearchOrder() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input placeholder='Search order #' value={query} onChange={(e) => setQuery(e.target.value)} className='rounded-3xl px-4 py-1.5 w-40 sm:w-72 transition-all bg-yellow-100'/>
+      <input
+        placeholder="Search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 placeholder:text-stone-400 duration-300 transition-all sm:w-64 sm:focus:w-72 focus:outline-none focus:ring focus:ring-yellow-300"
+      />
     </form>
-  )
+  );
 }
